fefe1ade8a63e2973ae63bc15380c982ce89d3c5
Sam Cross <samgcdev+git@gmail.com>
2021-08-19 19:53:50 +0100

    Initial commit

---
+++ Join-Sites.ps1
@@ -0,0 +1,41 @@
+﻿# Variables (CHANGE THESE!)
+## Site name for Configuration Manager
+$CMSiteName = '__CHANGE_ME__'
+$CMModuleLocation = 'C:\Program Files (x86)\ConfigMgr Console\bin\ConfigurationManager.psd1'
+
+# Definitions
+$scriptPath = $MyInvocation.MyCommand.Definition
+$langMode = $ExecutionContext.SessionState.LanguageMode
+
+# Check if correct language mode enabled
+# Usually, an incorrect language mode means the script is NOT elevated.
+If ($langMode -eq "ConstrainedLanguage") {
+    Write-Error "This script is running in Constrained Language mode - please launch an elevated PowerShell session and run this script from inside it."
+
+    exit 1
+}
+
+Write-Information "Importing SCCM module..."
+Import-Module $CMModuleLocation -ErrorVariable SCCMModuleGetError
+if ($SCCMModuleGetError) {
+    Write-Error "SCCM module is unavailable. Exiting..."
+    Exit 1
+}
+
+Write-Information "ConfigurationManager module version: $((Get-Module -Name ConfigurationManager).Version)"
+
+# Connect to site
+Write-Information "Connecting to site..."
+New-PSDrive -Name "P01" -PSProvider "CMSite" -Root $CMSiteName -Description "Primary site" -ErrorAction SilentlyContinue -ErrorVariable PSDriveGetError
+if ($PSDriveGetError) {
+    Write-Error "The necessary preperation steps have failed.`n`tThe connection may already exist, or you don't have the correct permissions to initiate it."
+    Write-Output "`n`nStart a new PowerShell session as an Administrator, and re-run this script. Exiting..."
+    Exit 1
+}
+
+Set-Location P01:
+Write-Information "Site Information: $(Get-CMSite)"
+
+Write-Output "Initialisation complete."
+
+Exit 0
---
+++ Launch-Admin-Apps.ps1
@@ -0,0 +1,39 @@
+﻿#### See LICENCE.md
+####
+
+# Variables (CHANGE ME!)
+$CMLocation = "C:\Program Files (x86)\ConfigMgr Console\bin\Microsoft.ConfigurationManagement.exe"
+
+# Definitions
+$scriptPath = $MyInvocation.MyCommand.Definition
+$langMode = $ExecutionContext.SessionState.LanguageMode
+
+Try {
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $isElevated = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+} catch {
+    $isElevated = $false
+}
+
+# Check if correct language mode enabled
+# Usually, an incorrect language mode means the script is NOT elevated, so we will try to relaunch anyway...
+If ($langMode -eq "ConstrainedLanguage" -or $isElevated -eq $false) {
+    Write-Warning "Check for elevation failed. Relaunching with elevated permissions..."
+
+    Start-Process -Verb RunAs powershell.exe -ArgumentList "& '$($scriptPath)'"
+    exit 0
+}
+
+# Start an administrator PowerShell session
+Start-Process -Verb RunAs powershell
+
+# Start AD Users and Computers
+Start-Process -Verb RunAs $env:SystemRoot\system32\dsa.msc
+
+# Start MECM
+# Note: Sometimes this will not be the correct file path, as the config manager will be in a different location
+#       If this is the case, just update the path below.
+Start-Process -Verb RunAs "'$($CMLocation)'"
+
+# Exit this session
+exit 0
